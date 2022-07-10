c8508a0ab8f8bae32c55181f01fbfa2b2bb7e488
John McWhirter <jhmcwhirter@gmail.com>
2022-07-10 11:48:40 -0500

    add magnitude, normalization, dot and cross products

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::ops;
 
 struct Tuple{ pub x: f64, pub y: f64, pub z: f64, pub w: f64 }
 impl Tuple {
-  
   pub fn new(x: f64, y: f64, z: f64, w: f64) -> Tuple {
     Tuple{x: x, y: y, z: z, w: w}
   }
@@ -12,12 +11,27 @@ impl Tuple {
   pub fn is_vector(&self) -> bool {
     self.w == 0.0
   }
-  pub fn equals(&self, t: Tuple) -> bool {
+  pub fn equals(&self, t: Self) -> bool {
     const EPSILON: f64 = 0.00001;
     (self.x - t.x).abs() < EPSILON && (self.y - t.y).abs() < EPSILON && (self.z - t.z).abs() < EPSILON && (self.w - t.w).abs() < EPSILON
   }
-  pub fn minus(&self, t: Tuple) -> Tuple {
-    Tuple::new(self.x - t.x, self.y - t.y, self.z - t.z, self.w - t.w)
+  pub fn magnitude(&self) -> f64 {
+    (f64::powf(self.x, 2.0) + f64::powf(self.y, 2.0) + f64::powf(self.z, 2.0) + f64::powf(self.w, 2.0)).sqrt()
+  }
+  pub fn normalize(&self) -> Self {
+    let m = self.magnitude();
+    Tuple::new(self.x / m, self.y / m, self.z / m, self.w /m) 
+  }
+  pub fn dot(&self, t: Tuple) -> f64 {
+    self.x * t.x + self.y * t.y + self.z * t.z + self.w * t.w
+  }
+  pub fn cross(&self, t: Tuple) -> Tuple {
+    Tuple::new(
+      self.y * t.z - self.z * t.y,
+      self.z * t.x - self.x * t.z,
+      self.x * t.y - self.y * t.x,
+      0.0 // vector
+    )
   }
 }
 impl ops::Add for Tuple {
@@ -36,6 +50,18 @@ impl ops::Neg for Tuple {
   type Output = Self;
   fn neg(self) -> Self::Output {
     Tuple::new(-self.x, -self.y, -self.z, -self.w)
+  }
+}
+impl ops::Mul<f64> for Tuple {
+  type Output = Self;
+  fn mul(self, _rhs: f64) -> Self::Output {
+    Tuple::new(self.x * _rhs, self.y * _rhs, self.z * _rhs, self.w * _rhs)
+  }
+}
+impl ops::Div<f64> for Tuple {
+  type Output = Self;
+  fn div(self, _rhs: f64) -> Self::Output {
+    Tuple::new(self.x / _rhs, self.y / _rhs, self.z / _rhs, self.w / _rhs)
   }
 }
 
@@ -73,7 +99,7 @@ fn equal_tuples_are_equal() {
   assert!(t1.equals(t2));
 }
 #[test]
-fn point_creates_points() {
+fn point_fn_creates_points() {
   let p = point(1.0, 2.0, 3.0);
   assert!(p.is_point())
 }
@@ -84,7 +110,7 @@ fn equal_points_are_equal() {
   assert!(p1.equals(p2));
 }
 #[test]
-fn vector_creates_vectors() {
+fn vector_fn_creates_vectors() {
   let v = vector(1.0, 2.0, 3.0);
   assert!(v.is_vector())
 }
@@ -128,6 +154,55 @@ fn subtracting_a_vector_from_the_zero_vector() {
 fn negating_a_tuple() {
   let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
   assert!((-t).equals(Tuple::new(-1.0, 2.0, -3.0, 4.0)));
+}
+#[test]
+fn multiplying_a_tuple_by_a_scalar() {
+  let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+  assert!((t * 3.5).equals(Tuple::new(3.5, -7.0, 10.5, -14.0)));
+}
+#[test]
+fn multiplying_a_tuple_by_a_fraction() {
+  let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+  assert!((t * 0.5).equals(Tuple::new(0.5, -1.0, 1.5, -2.0)));
+}
+#[test]
+fn divding_a_tuple_by_a_scalar() {
+  let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+  assert!((t / 2.0).equals(Tuple::new(0.5, -1.0, 1.5, -2.0)));
+}
+#[test]
+fn magnitude_of_vectors() {
+  let v1 = vector(1.0, 0.0, 0.0);
+  let v2 = vector(0.0, 1.0, 0.0);
+  let v3 = vector(0.0, 0.0, 1.0);
+  let v4 = vector(1.0, 2.0, 3.0);
+  let v5 = vector(-1.0, -2.0, -3.0);
+  assert_eq!(v1.magnitude(), 1.0);
+  assert_eq!(v2.magnitude(), 1.0);
+  assert_eq!(v3.magnitude(), 1.0);
+  assert_eq!(v4.magnitude(), (14.0_f64).sqrt());
+  assert_eq!(v5.magnitude(), (14.0_f64).sqrt());
+}
+#[test]
+fn normalizing_vectors() {
+  let v1 = vector(4.0, 0.0, 0.0);
+  let v2 = vector(1.0, 2.0, 3.0);
+  assert!(v1.normalize().equals(vector(1.0, 0.0, 0.0)));
+  assert!(v2.normalize().equals(vector(0.26726, 0.53452, 0.80178)));
+}
+#[test]
+fn dot_product_of_tuples() {
+  let v1 = vector(1.0, 2.0, 3.0);
+  let v2 = vector(2.0, 3.0, 4.0);
+  assert_eq!(v1.dot(v2), 20.0);
+}
+#[test]
+fn cross_product_of_vectors() {
+  let v1 = vector(1.0, 2.0, 3.0);
+  let v2 = vector(2.0, 3.0, 4.0);
+  assert!(v1.cross(v2).equals(vector(-1.0, 2.0, -1.0)));
+  let v2 = vector(2.0, 3.0, 4.0);
+  assert!(v2.cross(v1).equals(vector(1.0, -2.0, 1.0)));
 }
 
 fn main() {
