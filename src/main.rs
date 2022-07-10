97f7d87975126a859161ece80297d433d0a546f2
John McWhirter <jhmcwhirter@gmail.com>
2022-07-09 23:46:01 -0500

    implement tuple addition, subtraction, and negation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-const EPSILON: f64 = 0.00001;
-struct Tuple{ pub x: f64, pub y: f64, pub z: f64, pub w: f64 }
+use std::ops;
 
+struct Tuple{ pub x: f64, pub y: f64, pub z: f64, pub w: f64 }
 impl Tuple {
+  
   pub fn new(x: f64, y: f64, z: f64, w: f64) -> Tuple {
     Tuple{x: x, y: y, z: z, w: w}
   }
@@ -12,12 +13,32 @@ impl Tuple {
     self.w == 0.0
   }
   pub fn equals(&self, t: Tuple) -> bool {
+    const EPSILON: f64 = 0.00001;
     (self.x - t.x).abs() < EPSILON && (self.y - t.y).abs() < EPSILON && (self.z - t.z).abs() < EPSILON && (self.w - t.w).abs() < EPSILON
   }
-  pub fn plus(&self, t: Tuple) -> Tuple {
-    Tuple::new(self.x + t.x, self.y + t.y, self.z + t.z, self.w + t.w)
+  pub fn minus(&self, t: Tuple) -> Tuple {
+    Tuple::new(self.x - t.x, self.y - t.y, self.z - t.z, self.w - t.w)
   }
 }
+impl ops::Add for Tuple {
+  type Output = Self;
+  fn add(self, _rhs: Self) -> Self::Output {
+    Tuple::new(self.x + _rhs.x, self.y + _rhs.y, self.z + _rhs.z, self.w + _rhs.w)
+  }
+}
+impl ops::Sub for Tuple {
+  type Output = Self;
+  fn sub(self, _rhs: Self) -> Self::Output {
+    Tuple::new(self.x - _rhs.x, self.y - _rhs.y, self.z - _rhs.z, self.w - _rhs.w)
+  }
+}
+impl ops::Neg for Tuple {
+  type Output = Self;
+  fn neg(self) -> Self::Output {
+    Tuple::new(-self.x, -self.y, -self.z, -self.w)
+  }
+}
+
 fn point(x: f64, y: f64, z: f64) -> Tuple {
   Tuple::new(x, y, z, 1.0)
 }
@@ -25,68 +46,91 @@ fn vector(x: f64, y: f64, z: f64) -> Tuple {
   Tuple::new(x, y, z, 0.0)
 }
 
-
 #[test]
 fn tuple_with_w_eq_1_is_a_point() {
-  let tuple = Tuple::new(1.0, 2.0, 3.0, 1.0);
-  assert_eq!(tuple.x, 1.0);
-  assert_eq!(tuple.y, 2.0);
-  assert_eq!(tuple.z, 3.0);
-  assert_eq!(tuple.w, 1.0);
-  assert!(tuple.is_point());
-  assert!(!tuple.is_vector());
+  let t = Tuple::new(1.0, 2.0, 3.0, 1.0);
+  assert_eq!(t.x, 1.0);
+  assert_eq!(t.y, 2.0);
+  assert_eq!(t.z, 3.0);
+  assert_eq!(t.w, 1.0);
+  assert!(t.is_point());
+  assert!(!t.is_vector());
 }
 #[test]
 fn tuple_with_w_eq_0_is_a_vector() {
-  let tuple = Tuple::new(1.0, 2.0, 3.0, 0.0);
-  assert_eq!(tuple.x, 1.0);
-  assert_eq!(tuple.y, 2.0);
-  assert_eq!(tuple.z, 3.0);
-  assert_eq!(tuple.w, 0.0);
-  assert!(tuple.is_vector());
-  assert!(!tuple.is_point());
+  let t = Tuple::new(1.0, 2.0, 3.0, 0.0);
+  assert_eq!(t.x, 1.0);
+  assert_eq!(t.y, 2.0);
+  assert_eq!(t.z, 3.0);
+  assert_eq!(t.w, 0.0);
+  assert!(t.is_vector());
+  assert!(!t.is_point());
 }
 #[test]
 fn equal_tuples_are_equal() {
-  let tuple1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
-  let tuple2 = Tuple::new(1.0, 2.0, 3.0, 0.0);
-  assert!(tuple1.equals(tuple2));
+  let t1 = Tuple::new(1.0, 2.0, 3.0, 0.0);
+  let t2 = Tuple::new(1.0, 2.0, 3.0, 0.0);
+  assert!(t1.equals(t2));
 }
 #[test]
 fn point_creates_points() {
-  let point = point(1.0, 2.0, 3.0);
-  assert!(point.is_point())
+  let p = point(1.0, 2.0, 3.0);
+  assert!(p.is_point())
 }
 #[test]
 fn equal_points_are_equal() {
-  let point1 = point(1.0, 2.0, 3.0);
-  let point2 = point(1.0, 2.0, 3.0);
-  assert!(point1.equals(point2));
+  let p1 = point(1.0, 2.0, 3.0);
+  let p2 = point(1.0, 2.0, 3.0);
+  assert!(p1.equals(p2));
 }
 #[test]
 fn vector_creates_vectors() {
-  let vector = vector(1.0, 2.0, 3.0);
-  assert!(vector.is_vector())
+  let v = vector(1.0, 2.0, 3.0);
+  assert!(v.is_vector())
 }
 #[test]
 fn equal_vectors_are_equal() {
-  let vector1 = vector(1.0, 2.0, 3.0);
-  let vector2 = vector(1.0, 2.0, 3.0);
-  assert!(vector1.equals(vector2));
+  let v1 = vector(1.0, 2.0, 3.0);
+  let v2 = vector(1.0, 2.0, 3.0);
+  assert!(v1.equals(v2));
 }
 #[test]
 fn adding_two_tuples() {
-  let tuple1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
-  let tuple2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
-  assert!(tuple1.plus(tuple2).equals(Tuple::new(1.0, 1.0, 6.0, 1.0)));
+  let t1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
+  let t2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
+  assert!((t1 + t2).equals(Tuple::new(1.0, 1.0, 6.0, 1.0)));
+}
+#[test]
+fn subtracting_two_points() {
+  let p1 = point(3.0, 2.0, 1.0);
+  let p2 = point(5.0, 6.0, 7.0);
+  assert!((p1 - p2).equals(vector(-2.0, -4.0, -6.0)));
+}
+#[test]
+fn subtracting_a_vector_from_a_point() {
+  let p = point(3.0, 2.0, 1.0);
+  let v = vector(5.0, 6.0, 7.0);
+  assert!((p - v).equals(point(-2.0, -4.0, -6.0)));
+}
+#[test]
+fn subtracting_two_vectors() {
+  let v1 = vector(3.0, 2.0, 1.0);
+  let v2 = vector(5.0, 6.0, 7.0);
+  assert!((v1 - v2).equals(vector(-2.0, -4.0, -6.0)));
+}
+#[test]
+fn subtracting_a_vector_from_the_zero_vector() {
+  let zero = vector(0.0, 0.0, 0.0);
+  let v = vector(1.0, -2.0, 3.0);
+  assert!((zero - v).equals(vector(-1.0, 2.0, -3.0)));
+}
+#[test]
+fn negating_a_tuple() {
+  let t = Tuple::new(1.0, -2.0, 3.0, -4.0);
+  assert!((-t).equals(Tuple::new(-1.0, 2.0, -3.0, 4.0)));
 }
 
 fn main() {
-  let tuple = Tuple::new(1.0, 2.0, 3.0, 1.0);
-  println!("tuple = ({}, {}, {}, {})", tuple.x, tuple.y, tuple.z, tuple.w);
-  let point = point(1.0, -2.0, -3.5);
-  println!("point = ({}, {}, {})", point.x, point.y, point.z);
-  let vector = vector(-1.0, 2.6, 10.0);
-  println!("vector = ({}, {}, {})", vector.x, vector.y, vector.z);
+  
 }
 
