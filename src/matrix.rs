8623a32b7e616f1072beab61acb8c59cc7cdb232
John McWhirter <jhmcwhirter@gmail.com>
2022-09-30 15:58:29 -0500

    implement matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,13 @@
-//use std::ops;
+use std::ops;
 
 #[derive(Clone)]
 pub struct Matrix{pub m: Vec<Vec<f64>>}
 impl Matrix {
   pub fn equals(&self, m: Self) -> bool {
     const EPSILON: f64 = 0.00001;
+    if self.m.len() != m.m.len() || self.m[0].len() != m.m[0].len(){
+      return false;
+    }
     for (i, row) in self.m.iter().enumerate() {
       for (j, col) in row.iter().enumerate() {
         if col - m.m[i][j] >= EPSILON {
@@ -13,6 +16,28 @@ impl Matrix {
       }
     } 
     return true;
+  }
+}
+impl ops::Mul<Matrix> for Matrix {
+  type Output = Self;
+  fn mul(self, _rhs: Matrix) -> Self::Output {
+    let a = self.m;
+    let b = _rhs.m;
+    let mut m = Matrix{m: vec![
+      vec![0.0, 0.0, 0.0, 0.0],
+      vec![0.0, 0.0, 0.0, 0.0],
+      vec![0.0, 0.0, 0.0, 0.0],
+      vec![0.0, 0.0, 0.0, 0.0]
+    ]};
+    for i in 0..4 {
+      for j in 0..4 {
+        m.m[i][j] = a[i][0] * b[0][j] +
+                    a[i][1] * b[1][j] +
+                    a[i][2] * b[2][j] +
+                    a[i][3] * b[3][j];
+      } 
+    }
+    m
   }
 }
 
@@ -56,33 +81,55 @@ fn a_3x3_matrix_ought_to_be_representable() {
 }
 #[test]
 fn matrix_equality_with_identical_matrices() {
-  let m = Matrix{m: vec![
+  let a = Matrix{m: vec![
     vec![1.0, 2.0, 3.0, 4.0],
     vec![5.0, 6.0, 7.0, 8.0],
     vec![9.0, 8.0, 7.0, 6.0],
     vec![5.0, 4.0, 3.0, 2.0]
   ]};
-  let p = Matrix{m: vec![
+  let b = Matrix{m: vec![
     vec![1.0, 2.0, 3.0, 4.0],
     vec![5.0, 6.0, 7.0, 8.0],
     vec![9.0, 8.0, 7.0, 6.0],
     vec![5.0, 4.0, 3.0, 2.0]
   ]};
-  assert!(m.equals(p));
+  assert!(a.equals(b));
 }
 #[test]
 fn matrix_equality_with_different_matrices() {
-  let m = Matrix{m: vec![
+  let a = Matrix{m: vec![
     vec![1.0, 2.0, 3.0, 4.0],
     vec![5.0, 6.0, 7.0, 8.0],
     vec![9.0, 8.0, 7.0, 6.0],
     vec![5.0, 4.0, 3.0, 2.0]
   ]};
-  let p = Matrix{m: vec![
+  let b = Matrix{m: vec![
     vec![2.0, 3.0, 4.0, 5.0],
     vec![6.0, 7.0, 8.0, 9.0],
     vec![8.0, 7.0, 6.0, 5.0],
     vec![4.0, 3.0, 2.0, 1.0]
   ]};
-  assert!(!m.equals(p));
+  assert!(!a.equals(b));
+}
+#[test]
+fn multiplying_two_matrices() {
+  let a = Matrix{m: vec![
+    vec![1.0, 2.0, 3.0, 4.0],
+    vec![5.0, 6.0, 7.0, 8.0],
+    vec![9.0, 8.0, 7.0, 6.0],
+    vec![5.0, 4.0, 3.0, 2.0]
+  ]};
+  let b = Matrix{m: vec![
+    vec![-2.0, 1.0, 2.0, 3.0],
+    vec![3.0, 2.0, 1.0, -1.0],
+    vec![4.0, 3.0, 6.0, 5.0],
+    vec![1.0, 2.0, 7.0, 8.0]
+  ]};
+  let c = Matrix{m: vec![
+    vec![20.0, 22.0, 50.0, 48.0],
+    vec![44.0, 54.0, 114.0, 108.0],
+    vec![40.0, 58.0, 110.0, 102.0],
+    vec![16.0, 26.0, 46.0, 42.0]
+  ]};
+  assert!((a * b).equals(c));
 }
